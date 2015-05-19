68f460bbb9a131b33af89ccd3cb301e18604bd26
Ilya Zykov <ilya.zykov@icloud.com>
2015-05-19 10:19:31 +0300

    working version

--- polyhedraLab/source.py
+++ polyhedraLab/source.py
@@ -1,150 +1,71 @@
-from sympy import Matrix
+import numpy as np
+import sympy as sp
 
 
-#TODO
-def gauss( a, f, e, q ,r, perm, intarith, eps):
-    m = a.rows
-    n = a.cols
-
-    f.assign_eye(n) #TODO what is assign_eye?
-    e.resize(0, n) #TODO
-
-    q = transpose(a) #TODO
-
-    for i in range(min(q.cols, q.rows)):
-        q_pivot = abs(q[i][i]) #TODO
-        j_pivot = i
-        for j in range(i+1, m): #TODO
-            if abs(q[i][j] > q_pivot: #TODO
-                j_pivot = j
-                q_pivot = abs(q[i][j])
-    if q_pivot <= eps:
-        # it's a zero row
-        q.erase_row(i) #TODO
-        e.insert_row(e.rows, f.take_row(i)) #TODO
-        continue
-
-    if i != j_pivot:
-        q.swap_cols(i, j_pivot) #TODO swap?
-        perm.swap_els(i, j_pivot) #TODO swap?
-
-    if q[i][i] < 0:
-        q.mult_row(i, -1) #TODO
-        f.mult_row(i, -1) #TODO
-    #TODO ...
-    # branch test
-    print "test gauss"
+def get_matrix_B(cone_matrix): #TODO: rename
+    size = cone_matrix.rank()
+    B = cone_matrix[:size,:]
+    A = cone_matrix[size:,:]
+    if (B.rank() != size):
+        raise ValueError('Degenerate matrix')
+    return A, B
 
 
-#TODO
-def do_ddm( ineq, 
-            eq, 
-            ext, 
-            bas, 
-            dis, 
-            edges_ind, 
-            totalnumrays, 
-            totalnumedges, 
-            edgesflag, 
-            order, 
-            prefixed_order, 
-            graphadj, 
-            plusplus, 
-            intarith, 
-            eps,
-            logonstdout,
-            logfileflag,
-            logfile ):
-    print "test do_ddm"
+def adjacent(plus, minus): #TODO realize function!!!
+    #DEBUG: print plus
+    #DEBUG: print minus
+    return True
 
 
-def ddm( ineq, 
-         eq, 
-         ext, 
-         bas, 
-         dis, 
-         edges_ind, 
-         totalnumrays, 
-         totalnumedges, 
-         edgesflag, 
-         order, 
-         prefixed_order, 
-         graphadj, 
-         plusplus, 
-         intarith, 
-         eps,
-         logonstdout,
-         logfileflag,
-         logfile ):
-    if (eq.rows == 0):
-        do_ddm()#TODO
-    else:
-        bus_sub = Matrix()
-        f = Matrix()
-        q = Matrix()
+def step(new_row, Ut): #TODO: rename
+    aiu = new_row*Ut
 
-        m = ineq.rows
-        perm = []
-        for j in range(m):
-            perm.append(j)
-        gauss()#TODO
-        do_ddm()#TODO
+    uplus = []
+    uminus = []
+    uzero = []
 
-        ext = ext*bas_sub
-        bas = bas*bas_sub
+    uplusminus = []
 
-        ext_nrows = ext.rows
-        bas_nrows = bas.rows
+    for j in range(aiu.cols):
+        if aiu[j] > 0:
+            uplus.append(j)
+        elif aiu[j] < 0:
+            uminus.append(j)
+        else:
+            uzero.append(j)
 
-        for ii in range(ext_nrows):
-            if (intarith):
-                print "test intarith true"
-                #delta = gcd(ext.copy_row(ii))#TODO copy_row in cympy
-            else:
-                print "test intarith false"
-                #delta = firstnonzero(ext.copy_row(ii), eps)#TODO the same as below
-            #bas.div_row(ii, delta)#TODO div_row
-        print "test ddm"
+    for iplus in uplus:
+        for iminus in uminus:
+            if adjacent(Ut[:,iplus], Ut[:,iminus]):
+                newU = aiu[iplus]*Ut[:,iminus] - aiu[iminus]*Ut[:,iplus]
+                uplusminus.append(newU)
 
+    #DEBUG: print uminus[::-1]
+    for i in uminus[::-1]:
+        Ut.col_del(i)
 
-def main():
-    ineq = Matrix()
-    eq = Matrix()
-    ext = Matrix()
-    bas = Matrix()
-    dis = Matrix()
-    edges_ind = Matrix()
-    totalnumrays = 10
-    totalnumedges = 5
-    edgesflag = 1
-    order = 1
-    prefixed_order = 1
-    graphadj = 1
-    plusplus = 1
-    intarith = 1
-    eps = 0.1
-    logonstdout = 1
-    logfileflag = 1
-    logfile = ""
-    ddm(ineq, 
-        eq, 
-        ext, 
-        bas, 
-        dis, 
-        edges_ind, 
-        totalnumrays, 
-        totalnumedges,
-        edgesflag,
-        order,
-        prefixed_order,
-        graphadj,
-        plusplus,
-        intarith,
-        eps,
-        logonstdout,
-        logfileflag,
-        logfile)
+    #DEBUG: print np.matrix(Ut)
+
+    for i in uplusminus:
+        Ut = Ut.col_insert(Ut.rows, sp.Matrix([i]))
+
+    #DEBUG: print np.matrix(Ut)
+    return Ut
+
+def double_description(cone_matrix):
+    A, B = get_matrix_B(cone_matrix)
+    size = B.rank()
+
+    Ut = B.inv()
+
+    #DEBUG: print np.matrix(Ut)
+
+    for i in range(A.rows):
+        Ut = step(A[i,:], Ut)
+
+    #DEBUG: print np.matrix(Ut)
+
 
 if __name__ == "__main__":
-    main()
-
+    a = sp.Matrix([[1,-1,0],[1,1,0],[1,0,1],[1,0,-1],[1,0,0]])
+    double_description(a)
